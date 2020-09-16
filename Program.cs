85f958b160ef92b837e239b0e3dacc541f14f4ce
ronngo <ronngomn23@gmail.com>
2020-09-16 17:14:23 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GTNN2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int size;
+          int[] array;
+          do
+          {
+              Console.WriteLine("Nhap kich thuoc cho mang (toi da 20) :");
+              size = Int32.Parse(Console.ReadLine());
+              if (size > 20)
+                   Console.WriteLine("Vuot qua kich thuoc cho phep ");
+          } while (size > 20);
+
+          // nhập phần tử cho mảng
+          array = new int[size];
+          int i = 0;
+          while (i < array.Length)
+          {
+              Console.WriteLine("Nhap phan tu " + (i + 1) +" : ");
+              array[i] = Int32.Parse(Console.ReadLine());
+              i++;
+          }
+
+          // in ra phần tử cho mảng
+          Console.WriteLine("List :");
+          for (int j = 0; i < array.Length; j++)
+          {
+              Console.WriteLine(array[j] + "\t");
+          }
+          int min = MinValue(array);
+          Console.WriteLine("Phan tu nho nhat trong mang la :{0}", min);
+        }
+
+        static int MinValue(int[] array)
+        {
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+
+                }
+            }
+            return min;
+        }
+    }
+}
